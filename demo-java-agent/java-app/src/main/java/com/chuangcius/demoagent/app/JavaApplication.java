57da31d32da428fdcecadb2f157b92d43b98ed76
xugang <chuangcius@gmail.com>
2022-12-06 17:51:55 +0800

    add: demo-java-agent -> java-app

---
+++ demo-java-agent/java-app/src/main/java/com/chuangcius/demoagent/app/JavaApplication.java
@@ -0,0 +1,14 @@
+package com.chuangcius.demoagent.app;
+
+/**
+ * JavaApplication
+ *
+ * @author chuangcius
+ * @date 2022.12.06
+ */
+public class JavaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hi I am main.");
+    }
+}
